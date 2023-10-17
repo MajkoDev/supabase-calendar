199823cc2c4c8762757446a8bd6d8c8ed047b7df
Marian <hellomariandev@gmail.com>
2023-10-17 20:22:22 +0200

    Google Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
+import {
+  useSession,
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+
 function App() {
+  //TODO ==> Google Authentication
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  const { isLoading } = useSessionContext();
+
+  if (isLoading) {
+    return <></>;
+  }
+
+  const googleSignIn = async () => {
+    const {} = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        scopes: "https://www.googleapis.com/auth/calendar",
+      },
+    });
+  };
+  const googleSignOut = async () => {
+    await supabase.auth.signOut();
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-semibold text-center">Hello World</h1>
-    </>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      {session ? (
+        <>
+          <h2 className="font-semibold text-xl">
+            Hey there {session.user.email}
+          </h2>
+          <button
+            onClick={() => googleSignOut()}
+            className="p-4 border border-solid uppercase hover:bg-slate-100 rounded-lg"
+          >
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            onClick={() => googleSignIn()}
+            className="p-4 border border-solid uppercase hover:bg-slate-100 rounded-lg"
+          >
+            Sign In With Google
+          </button>
+        </>
+      )}
+    </div>
   );
 }
 
